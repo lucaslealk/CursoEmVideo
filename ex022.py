da19b381b7a82797b877500953d9583d60af9bbf
Lucas Leal <lucasleal.3011@gmail.com>
2025-10-14 18:53:59 -0300

    22

--- ex022.py
+++ ex022.py
@@ -4,8 +4,9 @@
 - Quantas letras ao todo (sem considerar espaços).
 - Quantas letras tem o primeiro nome.'''
 nome = str(input('Digite seu nome completo: ')).strip()
-print('Seu nome fica assim com o upper: ',nome.upper())
-print('Seu nome fica assim com o lower: ',nome.lower())
+print('Seu nome fica assim com o maiúsculas: ',nome.upper())
+print('Seu nome fica assim com o minúsculas: ',nome.lower())
 print('Seu nome tem ao todo {} letras'.format(len(nome) - nome.count(' ')))
 print('Seu primeiro nome é {} e ele tem {} letras.'.format(nome.split()[0], len(nome.split()[0])))
 
+  
