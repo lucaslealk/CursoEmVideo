f70f7a631daf73c5dde770aa6eaa3a5908b1d8f5
Lucas Leal <lucasleal.3011@gmail.com>
2025-10-18 11:00:01 -0300

    cores, texto e back

---
+++ cores.py
@@ -0,0 +1,23 @@
+cores = {'amarelo': '\033[93m', 
+         'vermelho': '\033[91m', 
+         'verde': '\033[92m', 
+         'azul': '\033[94m', 
+         'roxo': '\033[95m', 
+         'ciano': '\033[96m', 
+         'branco': '\033[97m', 
+         'reset': '\033[0m'}
+
+texto = {'negrito': '\033[1m',
+         'sublinhado': '\033[4m',
+         'inverso': '\033[7m'}
+
+back = {'amarelo': '\033[43m',
+        'vermelho': '\033[41m',
+        'verde': '\033[42m',
+        'azul': '\033[44m',
+        'roxo': '\033[45m',
+        'ciano': '\033[46m',
+        'branco': '\033[40m',
+        'reset': '\033[0m',
+        'cinza': '\033[47m'}
+
--- ex019.py
+++ ex019.py
@@ -2,10 +2,11 @@
 o quadro. Faça um programa que ajude ele, lendo o nome deles e 
 escrevendo o nome do escolhido.'''
 import random 
+import cores
 n1 = str(input('Digite o nome do primeiro aluno: '))
 n2 = str(input('DIgite o nome do segundo aluno: '))
 n3 = str(input('Digite o nome do terceiro aluno: '))
 n4 = str(input('Digite o nome do quarto aluno: '))
 lista = [n1, n2, n3, n4]
 escolhido = random.choice(lista) 
-print('O aluno escolhido foi {}'.format(escolhido))
+print('{}{}O aluno escolhido foi {}{}'.format(cores.cores['vermelho'], cores.back['amarelo'], escolhido, cores.cores['reset']))
