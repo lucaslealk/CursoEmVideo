17782c9ce4b2aee022a010b150e6e4dba978dc75
Lucas Leal <lucasleal.3011@gmail.com>
2025-10-20 16:44:01 -0300

    ex do 36 a 45

---
+++ ex041.py
@@ -0,0 +1,8 @@
+'''A confederação brasileira de natação precisa de um programa que 
+leia o ano de nascimento de um atleta e mostre sua categoria, de acordo 
+com a idade:
+- Até 9 anos: MIRIM
+- Até 14 anos: INFANTIL
+- Até 19 anos: JÚNIOR
+- Até 20 anos: SÊNIOR
+- Acima de 20 anos: MASTER'''
