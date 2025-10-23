e8a62d68704ee02a089db30a5e1b7d980612597a
Lucas Leal <lucasleal.3011@gmail.com>
2025-10-23 10:40:04 -0300

    ex 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56

---
+++ ex056.py
@@ -0,0 +1,4 @@
+'''Desenvolva um programa que leia o nome, idade e sexo de 4 pessoas. No final do programa, mostre:
+a) A média de idade do grupo.
+b) Qual é o nome do homem mais velho.
+c) Quantas mulheres têm menos de 20 anos.'''
