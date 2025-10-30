8e1d58809aca73f890a16bb239aea2ea05005633
Lucas Leal <lucasleal.3011@gmail.com>
2025-10-29 21:10:11 -0300

    ex 51

--- ex051.py
+++ ex051.py
@@ -1,1 +1,8 @@
 #Desenvolva um programa que leia o primeiro termo e a razão de uma PA. No final, mostre os 10 primeiros termos dessa progressão.
+print('\nVamos calcular uma Progressão Aritimética!')
+n1 = int(input('Primeiro termo: '))
+r = int(input('Razão: '))
+na = n1 + (11 - 1) * r
+for c in range(n1, na, r):
+    print(f'{c}', end = ' -> ')
+print('FIM')
